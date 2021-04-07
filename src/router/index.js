3e274b0676002b41e5f16a052c13dd2b37166eca
yuexingxing <670176656@qq.com>
2021-04-07 13:32:06 +0800

    登录路由逻辑处理

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,54 @@
 import {
-  createRouter,
-  createWebHashHistory
+	createRouter,
+	createWebHashHistory
 } from 'vue-router'
 import Home from '../views/home/Home.vue'
 import Login from '../views/login/Login.vue'
 import Register from '../views/register/Register.vue'
 
 const routes = [{
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  }
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: Login,
+		beforeEnter(to, from, next) {
+			console.log("router-beforeEnter", to, from);
+			var isLogin = localStorage.getItem("isLogin")
+			if (isLogin) {
+				next({
+					name: 'Home'
+				});
+			} else {
+				next();
+			}
+		}
+	},
+	{
+		path: '/register',
+		name: 'Register',
+		component: Register
+	}
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+	history: createWebHashHistory(),
+	routes
 })
 
 router.beforeEach((to, from, next) => {
-  console.log("router-beforeEach", to, from)
-  var isLogin = localStorage.getItem("isLogin")
-  if (isLogin || to.name === "Login" || to.name == "Register") {
-    next();
-  } else {
-    next({name: 'Login'});
-  }
+	console.log("router-beforeEach", to, from)
+	var isLogin = localStorage.getItem("isLogin")
+	if (isLogin || to.name === "Login" || to.name == "Register") {
+		next();
+	} else {
+		next({
+			name: 'Login'
+		});
+	}
 });
 
 export default router
