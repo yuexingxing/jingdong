7ff39f7cd7fff55e04c5a4d1cb9b3073d6ded6fb
yuexingxing <670176656@qq.com>
2021-04-13 22:33:43 +0800

    创作中心优化

--- src/util/request_get.js
+++ src/util/request_get.js
@@ -4,8 +4,8 @@ export const get = (url) => {
 	return new Promise((resolve, reject) => {
 		let token = localStorage.getItem('token')
 		axios.get(url, {
-			// baseURL: 'https://api.sancellvarymay.com/',
-			baseURL: 'http://t2wxapi.sancell.top/',
+			baseURL: 'https://api.sancellvarymay.com/',
+			// baseURL: 'http://t2wxapi.sancell.top/',
 			headers: {
 				'Content-Type': 'application/json',
 				'wx-token': token
--- src/views/homepage/AnchorFragment.vue
+++ src/views/homepage/AnchorFragment.vue
@@ -3,10 +3,31 @@
 		<img class="bg" src="../../assets/image_creationcenter.png" />
 		<div class="live-container">
 			<el-avatar class="avatar" :src="userData.userInfo.avatar" />
-			
 			<div class="space"></div>
 			<label class="live_title">我要直播</label>
 		</div>
+		<div class="nickname-container">
+			<label class="nickname-container__nickname">{{userData.userInfo.nickname}}</label>
+			<label class="nickname-container__label">三少高定</label>
+		</div>
+		<div class="fans">
+			<label class="fans__title">粉丝</label>
+			<label class="fans__count">{{userData.anchorInfo.attention_count}}</label>
+		</div>
+	</div>
+	<div class="tab">
+		<el-tabs class="tab__item" 
+		v-model="data.activeName" @tab-click="handleModelClick" stretch="true">
+			<el-tab-pane label="直播记录" name="first">
+				<ModelFragment :index="1" :tags="直播记录" />
+			</el-tab-pane>
+			<el-tab-pane label="关注" name="second">
+				<ModelFragment :index="2" :tags="关注" />
+			</el-tab-pane>
+			<el-tab-pane label="短视频" name="third">
+				<ModelFragment :index="3" :tags="短视频" />
+			</el-tab-pane>
+		</el-tabs>
 	</div>
 </template>
 
@@ -24,7 +45,12 @@
 	const UserInfoEffect = () => {
 
 		const userData = reactive({
-			userInfo: {}
+			userInfo: {
+				nickname: "三少"
+			},
+			anchorInfo: {
+				attention_count: 0
+			}
 		})
 
 		const getUserInfo = async (memId) => {
@@ -35,9 +61,19 @@
 			}
 		}
 
+		const getAnchorInfo = async (memId) => {
+			const result = await get("api/live/mlive/anchorDetail?mem_id=" + memId)
+			console.log(result)
+			if (result.ret === 'OK') {
+				userData.anchorInfo = result.content
+			}
+		}
+
+
 		return {
 			userData,
-			getUserInfo
+			getUserInfo,
+			getAnchorInfo
 		}
 	}
 
@@ -47,17 +83,25 @@
 			console.log("setup")
 			const {
 				userData,
-				getUserInfo
+				getUserInfo,
+				getAnchorInfo
 			} = UserInfoEffect()
 
+			const data = reactive({
+				activeName: 'second'
+			})
+
 			return {
+				data,
 				userData,
-				getUserInfo
+				getUserInfo,
+				getAnchorInfo
 			}
 		},
 		mounted() {
 			console.log("mounted")
 			console.log(this.$route.params)
+			this.getAnchorInfo(this.$route.params.anchorId)
 			this.getUserInfo(this.$route.params.anchorId)
 		}
 	}
@@ -73,8 +117,8 @@
 		width: .70rem;
 		height: .70rem;
 	}
-	
-	.live-container{
+
+	.live-container {
 		width: 100%;
 		position: absolute;
 		display: flex;
@@ -88,11 +132,12 @@
 		height: 4rem;
 		z-index: -1;
 	}
-	
-	.space{
+
+	.space {
 		flex: 1;
 	}
-	.live_title{
+
+	.live_title {
 		width: 1.73rem;
 		height: .36rem;
 		font-size: .16rem;
@@ -103,5 +148,64 @@
 		text-align: center;
 		line-height: .36rem;
 		margin-right: .5rem;
+	}
+
+	.nickname-container {
+		position: absolute;
+		display: flex;
+		flex-direction: row;
+		margin-top: 2.28rem;
+		margin-left: .15rem;
+		align-items: center;
+
+		&__nickname {
+			font-size: .17rem;
+			color: white;
+			text-align: center;
+			line-height: auto;
+			font-weight: 800;
+		}
+
+		&__label {
+
+			height: .16rem;
+			color: #B6A57B;
+			margin-left: .05rem;
+			border-radius: .2rem;
+			background: #333333;
+			font-size: .1rem;
+			padding-left: .08rem;
+			padding-right: .08rem;
+			text-align: center;
+			line-height: .16rem;
+		}
+	}
+
+	.fans {
+		display: flex;
+		flex-direction: row;
+		position: absolute;
+		margin-top: 2.58rem;
+		margin-left: .15rem;
+		align-items: center;
+
+		&__title {
+			font-size: .12rem;
+			color: white;
+		}
+
+		&__count {
+			font-size: .15rem;
+			color: white;
+			margin-left: .03rem;
+		}
+	}
+
+	.tab {
+		width: 100%;
+
+		&__item {
+			width: 100%;
+		}
 	}
 </style>
